69addccbd6a8c4d9b17fdee0bfc8ee3af2daaabf
Lossecretosdelexito <156558854+Lossecretosdelexito@users.noreply.github.com>
2024-04-21 12:27:17 -0300

    Actualizar ngrok_forward.sh

--- ngrok_forward.sh
+++ ngrok_forward.sh
@@ -1,5 +1,8 @@
+
 #!/bin/bash
+
+# Otorga permisos de ejecución al script si aún no los tiene
+chmod +x ngrok_forward.sh
 
 # Comando para ejecutar ngrok y reenviar el puerto 8888
 ./ngrok http 8888 --authtoken tu_authtoken_ngrok
-chmod +x ngrok_forward.sh
